aa33e72433a347559c172f55967f5f2b60c67318
alisalali <alis.alali@hotmail.com>
2023-12-14 21:50:36 +0300

    overall validate form with useEffect

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState(""); //setup a state for input value
   const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false); // to validate touched form input
+  // const [formIsValid, setFromIsValid] = useState(false); // form state validation
 
   // validate empty string
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+
+  /* useEffect approach way   
+ useEffect(() => {
+    if (enteredNameIsValid) {
+      setFromIsValid(true);
+      console.log("useffcet");
+    } else {
+      setFromIsValid(false);
+    }
+  }, [enteredNameIsValid]); 
+  */
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -30,7 +48,7 @@ const SimpleInput = (props) => {
 
     //rest form state
     setEnteredName("");
-    setEnteredNameIsTouched(false); //
+    setEnteredNameIsTouched(false);
   };
 
   // condition for touched state validation
@@ -60,7 +78,7 @@ const SimpleInput = (props) => {
         }
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
