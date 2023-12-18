765cec74aa58a86300291b2f963bf4c4074127f1
alisalali <alis.alali@hotmail.com>
2023-12-18 12:59:02 +0300

    useReducer with custom hook

--- src/components/hooks/use-validation.js
+++ src/components/hooks/use-validation.js
@@ -1,27 +1,67 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return {
+      value: action.value,
+      isTouched: state.isTouched,
+    };
+  }
+  if (action.type === "BLUR") {
+    return {
+      isTouched: true,
+      value: state.value,
+    };
+  }
+  if (action.type === "REST") {
+    return {
+      value: "",
+      isTouched: false,
+    };
+  }
+};
 
 const useValidation = (validationFunction) => {
-  const [valueInput, setValueInput] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  // const [valueInput, setValueInput] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const isValid = validationFunction(valueInput);
-  const hasError = !isValid && isTouched;
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+  const isValid = validationFunction(inputState.value);
+  const hasError = !isValid && inputState.isTouched;
 
   const inputChangedHandler = (event) => {
-    setValueInput(event.target.value);
+    // setValueInput(event.target.value);
+    dispatch({
+      type: "INPUT",
+      value: event.target.value,
+    });
   };
 
   const inputBlurChangedHandler = (event) => {
-    setIsTouched(true);
+    // setIsTouched(true);
+    dispatch({
+      type: "BLUR",
+    });
   };
 
   const rest = () => {
-    setValueInput("");
-    setIsTouched(false);
+    // setValueInput("");
+    // setIsTouched(false);
+    dispatch({
+      type: "REST",
+    });
   };
 
   return {
-    valueInput,
+    valueInput: inputState.value,
     isValid,
     hasError: hasError,
     inputChangedHandler,
